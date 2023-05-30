45073c967e20726545a7eb846f48d23c7594eca5
hu_tianyi <tianyi.hu@duke.edu>
2023-05-30 14:43:11 -0400

    Update datasplit.py

--- src/datasets/datasplit.py
+++ src/datasets/datasplit.py
@@ -60,7 +60,9 @@ class DataSplitter(object):
         elif split_type == "ShuffleSplit":
             return ShuffleSplitter(*args, **kwargs)
         elif split_type == "PoseErrorTimeSplit":
-            return PoseErrorSplitter(*args, **kwargs)
+            return PoseErrorTimeSplitter(*args, **kwargs)
+        elif split_type == "PoseErrorSequenceSplit":
+            return PoseErrorSequenceSplitter(*args, **kwargs)
         else:
             raise ValueError("DataSplitter for '{}' does not exist".format(split_type))
 
@@ -192,7 +194,7 @@ class ShuffleSplitter(DataSplitter):
 
 
 
-class PoseErrorSplitter(DataSplitter):
+class PoseErrorTimeSplitter(DataSplitter):
     """
     Returns randomized shuffled folds without requiring or taking into account the sample labels. Differs from k-fold
     in that not all samples are evaluated, and samples may be shared across validation sets,
@@ -275,5 +277,100 @@ class PoseErrorSplitter(DataSplitter):
         # Need to put into a nested list
         self.train_indices = [self.train_indices]
         self.val_indices = [self.val_indices]
+
+        return
+
+
+class PoseErrorSequenceSplitter(DataSplitter):
+    """
+    Cross Validation that Leave-One-Sequence Out
+    E.g., A0~A6 in the training set, A7 in the validation set
+    """
+
+    # First, leave the test set out (Here we set the test set size to 0)
+    # We can use the test_only mode (A jupyter notebook only for testing) later
+    def split_testset(self, test_ratio, random_state=1337):
+        """
+        Input:
+            test_ratio: ratio of test set with respect to the entire dataset. Should result in an absolute number of
+                samples which is greater or equal to the number of classes
+        Returns:
+            test_indices: numpy array containing the global datasets indices corresponding to the test set
+            test_labels: numpy array containing the labels corresponding to the test set
+        """
+
+        splitter = model_selection.ShuffleSplit(n_splits=1, test_size=test_ratio, random_state=random_state)
+        # get local indices, i.e. indices in [0, len(data_indices))
+        train_val_indices, test_indices = next(splitter.split(X=np.zeros(len(self.data_indices))))
+        # return global datasets indices and labels
+        self.train_val_indices = self.data_indices[train_val_indices]
+        self.test_indices = self.data_indices[test_indices]
+        if self.data_labels is not None:
+            self.train_val_labels = self.data_labels[train_val_indices]
+            self.test_labels = self.data_labels[test_indices]
+
+        return
+
+    def split_validation(self, n_splits, validation_ratio, random_state=1337):
+        """
+        Input:
+            n_splits: number of different, randomized and independent from one-another folds
+            validation_ratio: ratio of validation set with respect to the entire dataset. Should result in an absolute number of
+                samples which is greater or equal to the number of classes
+        Returns:
+            train_indices: iterable of n_splits (num. of folds) numpy arrays,
+                each array containing the global datasets indices corresponding to a fold's training set
+            val_indices: iterable of n_splits (num. of folds) numpy arrays,
+                each array containing the global datasets indices corresponding to a fold's validation set
+        """
+
+        '''
+        splitter = model_selection.ShuffleSplit(n_splits=n_splits, test_size=validation_ratio,
+                                                random_state=random_state)
+        # get local indices, i.e. indices in [0, len(train_val_labels)), per fold
+        train_indices, val_indices = zip(*splitter.split(X=np.zeros(len(self.train_val_indices))))
+        # return global datasets indices per fold
+        self.train_indices = [self.train_val_indices[fold_indices] for fold_indices in train_indices]
+        self.val_indices = [self.train_val_indices[fold_indices] for fold_indices in val_indices]
+        '''
+        split_dict = {}
+        self.train_indices = []
+        self.val_indices = []
+
+
+        for train_val_index in self.train_val_indices:
+            train_val_index_str = str(train_val_index)
+            # The index is in the following format
+            # 1AAA0BB0CCCC
+            # Where, 
+            # AAA  is the trajectory number
+            # BB   is the run (iteration) number
+            # CCCC is the time step (image index) number
+
+            # Take the first 4 charaters (trajectory) as key
+            key = train_val_index_str[:4]
+            if key not in split_dict:
+                split_dict[key] = [train_val_index]
+            else:
+                split_dict[key].append(train_val_index)
+
+        # for each sequence
+        self.train_indices = []
+        self.val_indices = []
+
+        for sequence in list(split_dict.keys()):
+            val_sequence = sequence
+            sequences = list(split_dict.keys())
+            sequences.remove(val_sequence)
+            train_sequences = sequences
+            
+            train_fold_indices = []
+            for train_sequence in train_sequences:
+                train_fold_indices += split_dict[train_sequence]
+            val_fold_indices = split_dict[val_sequence]
+            
+
+            self.train_indices.append(train_fold_indices)
+            self.val_indices.append(val_fold_indices)
 
         return
